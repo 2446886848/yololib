d2b16e415e5572fd9b85a3206757e62cc62c0a05
Walen Wu <walen.wu@hirede.com>
2017-01-04 11:58:41 +0800

    抽取核心代码为OC代码，便于其他项目调用

---
+++ Yololib.h
@@ -0,0 +1,15 @@
+//
+//  Yololib.h
+//  yololib
+//
+//  Created by walen on 17/1/4.
+//  Copyright © 2017年 test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Yololib : NSObject
+
++ (BOOL)injectFile:(NSString *)filePath libPath:(NSString *)libPath;
+
+@end
